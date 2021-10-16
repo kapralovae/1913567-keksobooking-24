95f80826722af1901a7cbadb79660169bc5fdedb
kapralovae <kapralov.ae@bk.ru>
2021-10-16 20:26:49 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -31,29 +31,16 @@ function getRandomFloat (from, to, lotNumber) {
 }
 
 
-let author = {
-  avatar : 'img/avatars/user' + `${getRandomInt(1,10)}`,
-}
-
 const ARR_FOR_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const ARR_TIME = ['12:00', '13:00', '14:00'];
 
-
-let arrPhotos = [
+const ARR_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-
-function getCountPhoto (){
-for (let i = 0; i < getRandomInt(0, 10); i++) { // Длина наполняемого массива от 0 до 10
-  let arrPhotosRandom = [];
-  arrPhotosRandom.push(arrPhotos[getRandomInt(0, 2)]); //Наполняем массив случайными значениями из arrPhotos
-  }
-};
-
-let arrForFeatures = [
+const ARR_FOR_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -61,19 +48,37 @@ let arrForFeatures = [
   'elevator',
   'conditioner'
 ];
+let author = {
+  avatar : 'img/avatars/user' + '0' + `${getRandomInt(1,10)}`,
+}
+
+let arrPhotosRandom = [];
+
+function getCountPhoto (){
+for (let i = 0; i < getRandomInt(0, 10); i++) { // Длина наполняемого массива от 0 до 10
+
+  arrPhotosRandom.push(ARR_PHOTOS[getRandomInt(0, 2)]); //Наполняем массив случайными значениями из ARR_PHOTOS
+  }
+  return arrPhotosRandom;
+};
 
 let arrFeatures = [];
 
 for (let i = 0; i < getRandomInt(1, 7); i++) {
-let cutValue = arrForFeatures.splice(getRandomInt(0, arrForFeatures.length - 1), 1);
+let cutValue = ARR_FOR_FEATURES.splice(getRandomInt(0, ARR_FOR_FEATURES.length - 1), 1);
 arrFeatures.push(cutValue[0]); // Наполняем массив arrFeature вырезая значения из arrForFeature, чтобы не было повторов
 
+}
+
+let location = {
+  lat : getRandomFloat(35.65000, 35.70000, 5),
+  lng : getRandomFloat(139.70000, 139.80000, 5),
 }
 
 let offer = {
   title : 'Welcome',
   address : `${location.lat} ` + `${location.lng}`,
-  price : getRandomInt(0, Infinity),
+  price : getRandomInt(0, 50000),
   type : ARR_FOR_TYPE[getRandomInt(0,4)],
   rooms : getRandomInt(1, 5),
   guests : getRandomInt(0, 6),
@@ -81,13 +86,10 @@ let offer = {
   cheeckout : ARR_TIME[getRandomInt(0, 2)],
   features : arrFeatures,
   description : 'Аренда',
-  photos : arrPhotosRandom,
+  photos : getCountPhoto (),
 }
 
-let location = {
-  lat : getRandomFloat(35.65000, 35.70000, 5),
-  lng : getRandomFloat(139.70000, 139.80000, 5),
-}
+
 
 let objectMassiv = [author, offer, location];
 let createMassive = [];
