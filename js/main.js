b67e868b1a54639a54121df0ea2f7902e88aedf5
kapralovae <kapralov.ae@bk.ru>
2021-10-16 19:50:01 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 function getRandomInt (from, to) {
   from = Math.ceil(from);
   to = Math.floor(to);
-// нужен ли в этом месте тернарный оператор?
-// (from <0 || to <0) ? return (alert('Вводите только положительные значения')) : return (Math.round(Math.random(from, to) * Math.max(from, to)))
   if (from <0 || to <0) {
     return alert('Вводите только положительные значения');
   }
@@ -13,12 +11,7 @@ function getRandomInt (from, to) {
     from = to;
     to = memory;
    }
-  return Math.floor(Math.random() * (to - from + 1) + from); // функция Math.random возвращает псевдослучайное число с плавающей запятой из диапазона [0, 1).
-
-  // Может показаться заманчивым использовать Math.round() для округления, но это может сделать распределение неравномерным, что может оказаться неприемлемым для ваших нужд.
-  // Каких нужд? Почему не стоит использовать Math.round()?
-  // Если написать return Math.round(Math.random() * Math.max(from, to)); не перезаписывав from = Math.ceil(from); и to = Math.floor(ro); , то результат тот же?
-  // фунция Math.round округляет до ближайшего целого числа Math.round(1.4) = 1 Math.round(1.5) = 2, поэтому диапозон станет [0, 1].
+  return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
 getRandomInt();
@@ -36,3 +29,74 @@ function getRandomFloat (from, to, lotNumber) {
 
   return Math.floor((((((Math.random() * lotNumber) + 1) / lotNumber) * (to - from)) + from) * lotNumber) / lotNumber;
 }
+
+
+let author = {
+  avatar : 'img/avatars/user' + `${getRandomInt(1,10)}`,
+}
+
+const ARR_FOR_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const ARR_TIME = ['12:00', '13:00', '14:00'];
+
+
+let arrPhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+
+function getCountPhoto (){
+for (let i = 0; i < getRandomInt(0, 10); i++) { // Длина наполняемого массива от 0 до 10
+  let arrPhotosRandom = [];
+  arrPhotosRandom.push(arrPhotos[getRandomInt(0, 2)]); //Наполняем массив случайными значениями из arrPhotos
+  }
+};
+
+let arrForFeatures = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+let arrFeatures = [];
+
+for (let i = 0; i < getRandomInt(1, 7); i++) {
+let cutValue = arrForFeatures.splice(getRandomInt(0, arrForFeatures.length - 1), 1);
+arrFeatures.push(cutValue[0]); // Наполняем массив arrFeature вырезая значения из arrForFeature, чтобы не было повторов
+
+}
+
+let offer = {
+  title : 'Welcome',
+  address : `${location.lat} ` + `${location.lng}`,
+  price : getRandomInt(0, Infinity),
+  type : ARR_FOR_TYPE[getRandomInt(0,4)],
+  rooms : getRandomInt(1, 5),
+  guests : getRandomInt(0, 6),
+  checkin : ARR_TIME[getRandomInt(0, 2)],
+  cheeckout : ARR_TIME[getRandomInt(0, 2)],
+  features : arrFeatures,
+  description : 'Аренда',
+  photos : arrPhotosRandom,
+}
+
+let location = {
+  lat : getRandomFloat(35.65000, 35.70000, 5),
+  lng : getRandomFloat(139.70000, 139.80000, 5),
+}
+
+let objectMassiv = [author, offer, location];
+let createMassive = [];
+
+function createAdvertisement (countAdvertisement) {
+for (let i = 0; i < countAdvertisement; i++) {
+  createMassive.push(objectMassiv.slice());
+  }
+  return createMassive; // На выходе функции получаем массив, где  [[author, offer, location],[author, offer, location],[]....]
+}
+
+createAdvertisement (10);
