70c851d982957e4f8dc16db5caea02a5160d3a58
kapralovae <kapralov.ae@bk.ru>
2021-10-26 22:27:20 +0300

    перенос в main.js добавление шаблона в map-canval

--- js/generate-data.js
+++ js/generate-data.js
@@ -58,6 +58,4 @@ function getArticle () {
   return fragment;
 }
 
-const mapCanvas = document.querySelector('#map-canvas');
-const copyCard = getArticle();
-mapCanvas.append(copyCard);
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-//import {getRandomInt, getRandomFloat} from './get-random-number.js';
-import './get-random-number.js';
-import './generate-data.js';
-import './data-and-get-object-massive.js';
+// import './get-random-number.js';
+// import './generate-data.js';
+// import './data-and-get-object-massive.js';
+import {getArticle} from './generate-data';
+
+const mapCanvas = document.querySelector('#map-canvas');
+const copyCard = getArticle();
+mapCanvas.append(copyCard);
